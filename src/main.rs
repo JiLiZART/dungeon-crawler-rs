4bfc978fc719d89f4867ba248e8fbf2c37a210fc
Nikolay Kostyurin <jilizart@gmail.com>
2022-01-01 16:39:39 +0200

    feat: map api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod map;
+mod player;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use dungeon_crawler::prelude::*;
+
 fn main() {
     println!("Hello, world!");
 }
