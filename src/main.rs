289fab1145a0d60c678b6b256dc1857e06cf549c
Nikolay Kostyurin <jilizart@gmail.com>
2022-01-02 12:41:54 +0200

    feat: player api

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,20 @@ mod prelude {
 
 use prelude::*;
 use map::*;
+use player::*;
 
 struct State {
     map: Map,
+    player: Player
 }
 
 impl State {
     fn new() -> Self {
         Self {
             map: Map::new(),
+            player: Player::new(
+                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            )
         }
     }
 }
@@ -27,7 +32,9 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
 
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
